a7316042ea24c263f0571856218c90ba78849724
root <root@DESKTOP-0RE296H.localdomain>
2019-12-23 23:05:36 +0530

    build

---
+++ client/build/precache-manifest.d2ffaae8a1cb881c9ac1f541be1bf425.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d052ae58053ce1ace7f3bcdcd95d467f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d52a4ce75747bdcedc85",
+    "url": "/static/css/main.bbb2fd74.chunk.css"
+  },
+  {
+    "revision": "b9b6d32f6badbd2a93e1",
+    "url": "/static/js/2.c57ea978.chunk.js"
+  },
+  {
+    "revision": "c12f3a257b3a26e0eaa14f5c53d4f1d8",
+    "url": "/static/js/2.c57ea978.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d52a4ce75747bdcedc85",
+    "url": "/static/js/main.925e2381.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
